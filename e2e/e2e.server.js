17377ad46f985b8c00ba0a54d3aa5393a895394c
N.pushkar <pushkar.anastasiia@gmail.com>
2024-12-15 19:43:30 +0300

    edited

--- e2e/e2e.server.js
+++ e2e/e2e.server.js
@@ -2,15 +2,6 @@ const webpack = require("webpack");
 const WebpackDevServer = require("webpack-dev-server");
 const config = require("../webpack.config.js");
 
-// const server = new WebpackDevServer(webpack(config), {});
-// server.listen(9000, 'localhost', (err) => {
-//   if (err) {
-//     return;
-//   }
-//   if (process.send) {
-//     process.send('ok');
-//   }
-// });
 const options = {
   host: "localhost",
   port: 9000,
